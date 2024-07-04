69f2fb13da93910ed1011f4e8c7b147dce6863b5
william calle <william.usp@hotmail.com>
2024-07-04 16:52:46 -0500

    fix: dockerization app

--- src/order/types/order.enum.ts
+++ src/order/types/order.enum.ts
@@ -1,7 +1,6 @@
-import { OrderStatus } from "@prisma/client";
 
 export const OrderStatuslist = [
-    OrderStatus.PENDING,
-    OrderStatus.CANCELLED,
-    OrderStatus.DELIVERED
+    "PENDING",
+    "CANCELLED",
+    "DELIVERED",
 ];
